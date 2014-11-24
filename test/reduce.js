43ce95361936e7b28e3cd727a6b15e7392794b7f
Dennis Charles Hackethal <dennis@ga.co>
2014-11-23 19:49:46 -0800

    Remove reduce

--- test/reduce.js
+++
@@ -1,5 +0,0 @@
-var expect = require('chai').expect;
-
-// Write a test for a method called 'reduce' that loops over an array
-// and returns the result of each operation reduced into a single value.
-// Then write the method that will make that test pass.
