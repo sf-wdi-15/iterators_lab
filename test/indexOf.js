07a86cd151edcdfdcb3094828b29a69282a010cc
Alexandra Connell <alexandraconnell@Alexandras-MacBook-Pro-6.local>
2014-11-24 21:31:24 -0800

    iteration hw

--- test/indexOf.js
+++ test/indexOf.js
@@ -3,10 +3,7 @@ var colors = require("colors");
 
 
 var indexOf = function (arr, value) {
-  // some code here to find the index of 
-  //  value in the arr.
-  // if the value isn't in the arr it 
-  //  should return -1
+  return arr.indexOf(value);
 };
 
 // Write assertions for a method called 'indexOf' that returns the index of
@@ -18,12 +15,12 @@ var indexOf = function (arr, value) {
   // Write an assertion for `indexOf` that 
   //  checks it returns -1 if value isn't 
   //  in the array.
-  assert.equal()
+  assert.equal(indexOf(testArr,10),-1,"That value is in the array".red);
 
 
   // Write an assertion for `indexOf` that 
   //  checks it returns the index of a 
   //  value in the array
-  assert.equal()
+  assert.equal(indexOf(testArr,6),1,"Not equal".red);
 
 console.log("IndexOf is working!".cyan);
